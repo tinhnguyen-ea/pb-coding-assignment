227b0c83bc654ee167d9aece34c30c9ff57f0b12
tinh.nguyen <tinhnguyen.qn.vn@gmail.com>
2025-12-21 02:26:06 +0800

    feat: [PB-000] Add fx service

---
+++ fx/package.go
@@ -0,0 +1,3 @@
+// Package fx provides a real time or historical currency metadata and exchange rate service.
+// for the sake of simplicity, we will use a fixed data for integration testing purposes.
+package fx
